0bf3374b07481f277d1b84bdda48726bb819069b
Andhitia Rama <andhitia.r@gmail.com>
2023-05-06 07:40:02 +0700

    [ssi_bill_of_service_project] 14.0.1.0.0

---
+++ ssi_bill_of_service_project/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Bill of Service - Project Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_bill_of_service",
+        "ssi_task_work_log",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/bill_of_service_views.xml",
+    ],
+    "demo": [],
+    "images": [],
+}
---
+++ ssi_bill_of_service_project/models/bill_of_service.py
@@ -0,0 +1,31 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class BillOfService(models.Model):
+    _name = "bill_of_service"
+    _inherit = ["bill_of_service"]
+
+    task_ids = fields.One2many(
+        string="Tasks",
+        comodel_name="bill_of_service.task",
+        inverse_name="bos_id",
+    )
+    total_work_estimation = fields.Float(
+        string="Total Work Estimation",
+        compute="_compute_total_work_estimation",
+        store=True,
+    )
+
+    @api.depends(
+        "task_ids.work_estimation",
+    )
+    def _compute_total_work_estimation(self):
+        for record in self:
+            result = 0.0
+            for task in record.task_ids:
+                result += task.work_estimation
+            record.total_work_estimation = result
