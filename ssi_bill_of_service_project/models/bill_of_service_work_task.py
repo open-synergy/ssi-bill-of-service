43340d6689036f9d44e23f46ee4fcbe1bf1b3a23
Andhitia Rama <andhitia.r@gmail.com>
2023-05-06 07:40:02 +0700

    [ssi_bill_of_service_project] 14.0.1.0.0

---
+++ ssi_bill_of_service_project/models/bill_of_service_work_task.py
@@ -0,0 +1,80 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class BillOfServiceTask(models.Model):
+    _name = "bill_of_service.task"
+    _description = "Bill Of Service Task"
+
+    sequence = fields.Integer(
+        string="Sequence",
+        required=True,
+        default=10,
+    )
+    bos_id = fields.Many2one(
+        string="Bill Of Service",
+        comodel_name="bill_of_service",
+        required=True,
+        ondelete="cascade",
+    )
+    name = fields.Char(
+        string="Task Summary",
+        required=True,
+    )
+    type_id = fields.Many2one(
+        string="Type",
+        comodel_name="task.type",
+        required=True,
+    )
+    type_category_id = fields.Many2one(
+        string="Type Category",
+        comodel_name="task.type_category",
+        related="type_id.category_id",
+        store=True,
+    )
+    difficulty = fields.Selection(
+        string="Difficulty",
+        index=True,
+        selection=[
+            ("0", "Low"),
+            ("1", "Medium"),
+            ("2", "High"),
+            ("3", "Very High"),
+        ],
+        default="0",
+        required=True,
+    )
+    num_of_parellel = fields.Integer(
+        string="Num of Parallel Task",
+        required=True,
+        default=1,
+    )
+    work_estimation = fields.Float(
+        string="Work Estimation",
+    )
+
+    @api.onchange(
+        "type_id",
+        "difficulty",
+        "num_of_parellel",
+    )
+    def onchange_work_estimation(self):
+        self.work_estimation = 0.0
+        if self.type_id:
+            standard_work_estimation = self.type_id.work_estimation
+            field_name = "work_estimation_offset_" + self.difficulty
+            offset_work_estimation = getattr(self.type_id, field_name)
+            self.work_estimation = (
+                standard_work_estimation + offset_work_estimation
+            ) * self.num_of_parellel
+
+    @api.onchange(
+        "type_id",
+    )
+    def onchange_name(self):
+        self.name = False
+        if self.type_id:
+            self.name = self.type_id.name
