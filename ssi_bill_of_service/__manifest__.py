bed27b50502ef2ee5a18cc2b2bf8aed72bdbc59c
Andhitia Rama <andhitia.r@gmail.com>
2023-05-06 07:39:18 +0700

    [ssi_bill_of_service] 14.0.1.0.0

---
+++ ssi_bill_of_service/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Bill of Service - Project Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_product",
+    ],
+    "data": [
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "views/bill_of_service_views.xml",
+        "views/bill_of_service_type_views.xml",
+    ],
+    "demo": [],
+    "images": [],
+}
---
+++ ssi_bill_of_service/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    bill_of_service,
+    bill_of_service_type,
+)
